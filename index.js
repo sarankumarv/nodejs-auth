85e542dffac3f88b4a3b4b7d2898a5af5a56f18d
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-05-07 17:32:57 -0700

    add modules

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let express = require('express')
 let morgan = require('morgan')
+let bodyParser = require('body-parser')
+let cookieParser = require('cookie-parser')
+let session = require('express-session')
+let passport = require('passport')
 
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 8000
 
 let app = express()
 
+app.set('view engine', 'ejs')
 app.listen(PORT, () => console.log(`Listening @ http://127.0.0.1:${PORT}`))
+app.get('/', (req, res) => res.render('index.ejs', {}))
+
